0919bf1bd369684de0672c312bedc16354f3e9df
Arda Firdaus Ramadhan <ardafirdausr@gmail.com>
2021-03-23 17:31:18 +0700

    add bootstrap

--- handler.go
+++ handler.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
+	"path"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	switch r.Method {
+	case http.MethodGet:
+		{
+			t := template.Must(template.ParseFiles(
+				path.Join("views", "_header.html"),
+				path.Join("views", "_footer.html"),
+				path.Join("views", "index.html"),
+			))
+
+			data := map[string]interface{}{}
+			data["Title"] = "Index"
+			err := t.ExecuteTemplate(w, "index", data)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+		}
+	default:
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
 }
